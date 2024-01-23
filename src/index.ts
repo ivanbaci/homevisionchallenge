43f5df5c9b51c16db3bb6fa508b6b8becb077a14
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-01-23 20:16:58 -0300

    feat: :sparkles: Call fetchHouses from main and print the data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+import { fetchHouses } from './apiClient';
+import { House } from './types/house.type';
+
+const main = async () => {
+  try {
+    const houses: House[] = await fetchHouses();
+    for (const house of houses) {
+      console.log('house:', house);
+    }
+  } catch (error) {
+    console.error(`Error fetching houses:`, error);
+  }
+};
+
+main().catch(error => {
+  console.error('Error:', error);
+});
