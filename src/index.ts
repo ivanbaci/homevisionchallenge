fad3aaebea859462e13df985d2f5badec28bb3ce
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-01-23 22:12:55 -0300

    feat: :sparkles: Call downloadPhoto for each received house

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import { fetchHouses } from './apiClient';
+import { downloadPhoto } from './photoDownloader';
 import { House } from './types/house.type';
 
 const main = async () => {
   try {
     const houses: House[] = await fetchHouses();
-    for (const house of houses) {
-      console.log('house:', house);
-    }
+    console.log(`Fetched ${houses.length} houses`);
+
+    const downloadPromises = houses.map(house => {
+      const filename = `${house.id}-${house.address}`;
+      return downloadPhoto(house.photoURL, filename, 'photos').catch(error =>
+        console.error(`Error downloading photo for house ${house.id}:`, error)
+      );
+    });
+
+    await Promise.all(downloadPromises);
   } catch (error) {
     console.error(`Error fetching houses:`, error);
   }
